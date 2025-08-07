1dc3ce52b8eeeef850202e653c31c7b47f6479ea
SumitSingh69 <issomeonein1234@gmail.com>
2025-08-07 15:26:23 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 const express = require('express');
+
+const app = express();
+
+app.use("/profile12", (req, res, next) => {
+    res.send('Profile middleware is working!');
+})
+
+app.use("/test", (req, res) => {
+    res.send('Test endpoint is working!');
+});
+
+app.use("/profile",(req, res) =>{
+    res.send('Profile endpoint is working!');
+})
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
