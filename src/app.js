a9f11d8674b43ab5da8b103e54438d157537dd2a
SumitSingh69 <issomeonein1234@gmail.com>
2025-08-14 11:03:27 +0530

    handling different routing

--- src/app.js
+++ src/app.js
@@ -2,17 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use("/profile12", (req, res, next) => {
-    res.send('Profile middleware is working!');
-})
 
-app.use("/test", (req, res) => {
-    res.send('Test endpoint is working!');
+app.get("/profile/:username", (req, res) => {
+    console.log(req.params);
+    res.send(req.params.username + " Profile page accessed");
 });
 
-app.use("/profile",(req, res) =>{
-    res.send('Profile endpoint is working!');
-})
 app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
