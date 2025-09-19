baa55fea5493d972a652d399a1fa30e97a5f3e59
SumitSingh69 <issomeonein1234@gmail.com>
2025-09-19 12:00:08 +0530

    added feed api and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userAuth = require("../middlewares/userMiddleware");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 const userRouter = express.Router();
 
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
@@ -42,6 +43,52 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.status(200).send({
       message: "Connections fetched successfully",
       allConnections,
+    });
+  } catch (err) {
+    throw err;
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const { page = 1, limit = 3 } = req.query;
+    const userId = req.user._id;
+    const existingConnections = await ConnectionRequest.find({
+      $or: [{ fromUserId: userId }, { toUserId: userId }],
+    });
+    const otherUserIds = existingConnections.map((connection) => {
+      if (connection.fromUserId.toString() === userId.toString()) {
+        return connection.toUserId;
+      }
+      return connection.fromUserId;
+    });
+
+    otherUserIds.push(userId);
+    //find the count of all users except the ones in otherUserIds
+    const totalFeedUsersCount = await User.countDocuments({
+      _id: { $nin: otherUserIds },
+    });
+    const totalPages = Math.ceil(totalFeedUsersCount / limit);
+    if (page > totalPages) {
+      throw new Error("No more users to show");
+    }
+    if (limit > totalFeedUsersCount) {
+      throw new Error("Limit exceeds total users count");
+    }
+
+    const feedUsers = await User.find({
+      _id: { $nin: otherUserIds },
+    })
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .select("firstName midName lastName");
+    res.status(200).send({
+      message: "Feed users fetched successfully",
+      feedUsers,
+      currentPage: page,
+      totalPages,
+      isNextPage: page < totalPages ? true : false,
+      isPrevPage: page > 1 ? true : false,
     });
   } catch (err) {
     throw err;
